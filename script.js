e055e5b7372ee58c52bab58fc2baae6e7bdd9e40
MicChowaniec <80547295+MicChowaniec@users.noreply.github.com>
2022-09-22 11:25:25 +0200

    Add files via upload
    Update

--- script.js
+++ script.js
@@ -1,28 +1,75 @@
-const loadText = document.querySelector('.loading-text');
-const bg = document.querySelector('.main-image');
-const slideRight = document.querySelector('.slider-container');
-const slideLeft = document.querySelector('.left-slide');
-const upButton = document.querySelector('.up-button');
-const downButton = document.querySelector('.down-button');
-const slidesLength = slideRight.querySelectorAll('.div').length;
+const loadText = document.querySelector('.loading-text')
+const mainImage = document.querySelector('.main-image')
+
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
 
 
 
-let int = setInterval(blurring,30)
+
+
+
+
+
+
+let activeSlideIndex = 0
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) =>
+{
+    const sliderHeight = sliderContainer.clientHeight;
+    if (direction =='up'){
+    activeSlideIndex++
+    
+        if(activeSlideIndex > slidesLength - 1)
+        {
+            activeSlideIndex =0
+        }
+    
+    slideRight.style.transform =`translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform =`translateY(${activeSlideIndex * sliderHeight}px)`
+    }
+    else if (direction =='down')
+    {
+        activeSlideIndex--
+    
+        if(activeSlideIndex < 0 )
+        {
+            activeSlideIndex =slidesLength -1
+        }
+    
+    slideRight.style.transform =`translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform =`translateY(${activeSlideIndex * sliderHeight}px)`
+    }
+}
+
+    
+    
+
+    
 
 let load = 0
+let int = setInterval(blurring,30)
 function blurring()
 {
+    
+    
     load+=5
-    if(load>99)
+    if(load>95)
     {
+        
         clearInterval(int)
     }
 
     loadText.innerText = `${load}%`
     loadText.style.opacity = (100-load)/100
-    bg.style.filter = `blur(${30*(100-load)/100}px)`
+    mainImage.style.filter = `blur(${30*(100-load)/100}px)`
 }
 
-let activeSlideIndex = 0;
-slideLeft.style.top = `-${(slidesLength - 1) *100}vh`
